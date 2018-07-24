99ca2259e5024022b090498063397d707712c50b
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-07-24 11:42:50 -0400

    Added update feature

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
-	http = require('http').Server(app);
+	http = require('http').Server(app),
+	methodOverride = require('method-override');
 
 // App config
 mongoose.connect('mongodb://localhost:27017/codelike_blog', { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // Mongoose/model confi
 var blogSchema = new mongoose.Schema({
@@ -61,6 +63,29 @@ app.get("/blog/:id", function(req, res){
 			res.render("show", {blog: foundBlog});
 		}
 	})
+});
+// EDIT Route
+app.get("/blog/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("edit", {
+				blog: foundBlog
+			});
+		}
+	});
+});
+
+// UPDATE Route
+app.put("/blog/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blog");
+		}else{
+			res.redirect("/blog/"+req.params.id);
+		}
+	});
 });
 
 http.listen(3000, function(){
